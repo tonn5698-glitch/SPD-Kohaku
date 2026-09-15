cfdf458cb877b715aa9443b5aa360221b8b2f5c2
tonn5698s-agent <tonn5698s-agent@users.noreply.github.com>
2026-09-15 01:18:36 +0000

    fix: revert ScrollOfHint energy deduction + add translation note

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/ScrollOfHint.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/scrolls/ScrollOfHint.java
@@ -211,11 +211,6 @@ public class ScrollOfHint extends Scroll {
 		SpellSprite.show(curUser, SpellSprite.MAP);
 		Sample.INSTANCE.play(Assets.Sounds.READ);
 		
-		// Deduct energy when scroll is used
-		if (energy > 0) {
-			energy--;
-		}
-		
 		readAnimation();
 	}
 	
